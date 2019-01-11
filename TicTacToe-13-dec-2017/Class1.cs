6199ff4a4cd5bb5f46ae85dee20cdb1c98cbdc78
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-11 11:45:00 +0100

    Init

---
+++ TicTacToe-13-dec-2017/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe_13_dec_2017 {
+    class Class1 {
+    }
+}
---
+++ TicTacToe-13-dec-2017/Player.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe_13_dec_2017 {
+    public class Player {
+    }
+}
